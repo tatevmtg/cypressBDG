2982e58831479f962a1ec4a2097155281fd76201
Mkhitaryan Tatev <tatevmtg>
2025-01-05 12:48:52 +0400

    broken-images

--- cypress/e2e/brokenImages.cy.js
+++ cypress/e2e/brokenImages.cy.js
@@ -1,3 +1,4 @@
+import { find } from "lodash";
 import { brokenImages } from "../Pages/addRemoveElements";
 import { basePage } from "../Pages/Base";
 
@@ -8,9 +9,26 @@ describe ('Broken_Images', () => {
         cy.url().should('include', 'broken_images')
     })
 
-    it.only ('Verify existence of images on the page', () => {
-        cy.visit ('/broken_images/')
-
+    it ('Verify existence of images on the page', () => {
+        cy.visit ('/broken_images')
+        cy.get('.example')
+        .find('img')
+        .should('be.visible')
     })
-// test case img size 120*90
+
+    it ('Verify image width', () => {
+        cy.visit ('/broken_images')
+        cy.get('.example')
+        .find('img')
+        .should('have.css', 'width', '120px')
+        .should('have.css', 'height', '90px')
+    })
+
+    it.only ('Verify broken images on the page', () => {
+        cy.visit ('/broken_images')
+        cy.get('.example')
+        .find('img')
+        .should('have.prop', 'naturalWidth')
+        .should('be.greaterThan', 0)
+    })
 })
