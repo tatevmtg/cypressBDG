05cc641ea72ddee7bf008f41ddc785375d953810
Mkhitaryan Tatev <tatevmtg>
2025-01-12 23:18:04 +0400

    forgotpassword

--- cypress/e2e/A_B_Testing.cy.js
+++ cypress/e2e/A_B_Testing.cy.js
@@ -1,14 +1,17 @@
 import { abTesting } from "../Pages/A_B_Testing";
 import { basePage } from "../Pages/Base";
 
-describe('A_B_Testing', () => {   //test suit
+describe("A_B_Testing", () => {
   before(() => {
-    cy.visit('/')
-  })
-  
-  it('Verify content in the page', () => {   //test case title
-    cy.visit('https://the-internet.herokuapp.com');
-    cy.contains('A/B Testing').click();
-    cy.get(basePage.LOCATORS.content).should('contain', abTesting.NAMES.contentText)
-  })
-})
+    cy.visit("/");
+  });
+
+  it("Verify content in the page", () => {
+    cy.visit("https://the-internet.herokuapp.com");
+    cy.contains("A/B Testing").click();
+    cy.get(basePage.LOCATORS.content).should(
+      "contain",
+      abTesting.NAMES.contentText
+    );
+  });
+});
--- cypress/e2e/forgotPassword.cy.js
+++ cypress/e2e/forgotPassword.cy.js
@@ -1,16 +1,38 @@
-describe ('Forgot Password', () => {
-    before(() => {
-        cy.visit('/')
-      })
+import { forgotPassword } from "../Pages/forgotPassword";
+import { uniqueEmails } from "../utils/function";
 
-    it ('Verify the existence of Forgot password page', () => {
-        cy.contains('Forgot Password').click()
-        cy.url().should('include', 'forgot_password');
-        cy.get('.row').should('include', 'E-mail')
-        cy.get('email').should('be.empty')
-        cy.get('#form_submit').should('have.css', 'color', '#2BA6CB', 'text', 'italic')
-    })
+forgotPassword;
+const emailName = uniqueEmails(10);
+let valueName = "";
 
-    
-
-})
+describe("Forgot Password", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.contains("Forgot Password").click();
+  });
+  it("Verify the existence of Forgot password page", () => {
+    cy.url().should("include", "forgot_password");
+    cy.get(".example").should("contain", "Forgot Password");
+    cy.get(".row").find("label").should("have.text", "E-mail");
+    cy.get("#email").should("be.empty");
+    cy.get("#form_submit").should(
+      "have.css",
+      "background-color",
+      "rgb(43, 166, 203)",
+      "font-style: italic"
+    );
+  });
+  it("Verify unique emails input", () => {
+    cy.get("#email").type(emailName);
+    cy.get("#email")
+      .invoke("val")
+      .then((val) => {
+        valueName = val;
+      });
+    cy.then(() => [expect(emailName).to.eq(valueName)]);
+  });
+  it.only("Verify clicking Retrieve password results internat server error", () => {
+    cy.get("#form_submit").click();
+    cy.get("h1").should("contain", "Internal Server Error");
+  });
+});
