72f843d5e3ba0aece442d0b8fcc097390a18ee8f
Mkhitaryan Tatev <tatevmtg>
2025-01-20 00:18:53 +0400

    refactor only

--- cypress/e2e/dropdown.cy.js
+++ cypress/e2e/dropdown.cy.js
@@ -1,33 +1,25 @@
-describe ('Dropdown', () => {
-    it ('Verify the existence of Dropdown page', () => {
-        cy.visit ('/')
-        cy.contains('Dropdown').click()
-        cy.url().should('include', 'dropdown')
-    })
+describe("Dropdown", () => {
+  it("Verify the existence of Dropdown page", () => {
+    cy.visit("/");
+    cy.contains("Dropdown").click();
+    cy.url().should("include", "dropdown");
+  });
 
-    it.only ('Verify the dropdown menu on the page', () => {
-        cy.visit ('/dropdown')
-        cy.get('.example')
-        .find('#dropdown')
-        .should('be.visible')
-    })
+  it("Verify the dropdown menu on the page", () => {
+    cy.visit("/dropdown");
+    cy.get(".example").find("#dropdown").should("be.visible");
+  });
 
-    it('Verify the 2 dropdown options', () => {
-        cy.visit ('/dropdown')
-        cy.get('#dropdown').select('Option 1')
-        .should('have.value', '1')
-        cy.get('#dropdown').select('Option 2')
-        .should('have.value', '2')
-    })
-    
-    it ('Verify there are 3 dropdown options', () => {
-        cy.visit ('/dropdown')
-        cy.get('#dropdown').select('Option 1')
-        .should('have.value', '1')
-        cy.get('#dropdown').select('Option 2')
-        .should('have.value', '2')
-        cy.get('#dropdown').select('Option 3')
-        .should('have.value', '3')
-    })
+  it("Verify the 2 dropdown options", () => {
+    cy.visit("/dropdown");
+    cy.get("#dropdown").select("Option 1").should("have.value", "1");
+    cy.get("#dropdown").select("Option 2").should("have.value", "2");
+  });
 
-})
+  it("Verify there are 3 dropdown options", () => {
+    cy.visit("/dropdown");
+    cy.get("#dropdown").select("Option 1").should("have.value", "1");
+    cy.get("#dropdown").select("Option 2").should("have.value", "2");
+    cy.get("#dropdown").select("Option 3").should("have.value", "3");
+  });
+});
