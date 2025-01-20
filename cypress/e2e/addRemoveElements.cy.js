dc300ec0ea0529d6c4bd9e75ad298c619c985241
Mkhitaryan Tatev <tatevmtg>
2025-01-20 22:41:08 +0400

    add remove refactoring

--- cypress/e2e/addRemoveElements.cy.js
+++ cypress/e2e/addRemoveElements.cy.js
@@ -2,59 +2,40 @@ import { addRemoveElements } from "../Pages/addRemoveElements";
 import { basePage } from "../Pages/Base";
 
 describe("Add_Remove_Elements", () => {
-  it('Verify the existence of "Add/Remove Element" page', () => {
+  beforeEach(() => {
     cy.visit("/");
-    cy.contains("Add/Remove Elements").click();
-    cy.url().should("include", "/add_remove_elements/");
+    cy.contains(addRemoveElements.NAMES.contentText).click();
   });
 
   it("Verify the button color", () => {
-    cy.visit("/add_remove_elements/");
+    cy.url().should("include", addRemoveElements.NAMES.urlPath);
     cy.get(basePage.LOCATORS.content).should(
       "contain",
       addRemoveElements.NAMES.contentText,
       "have.css",
       "background-color",
-      "#2ba6cb"
+      addRemoveElements.COLORS.blue
     );
   });
 
   it('Check "Add Element" button', () => {
-    cy.visit("/add_remove_elements/");
-    cy.get(".example")
+    cy.get(addRemoveElements.LOCATORS.addElementButton)
       .find("button")
       .then((btn) => {
-        expect(btn).to.contain("Add Element");
-      });
-  });
-
-  it('Verify "Delete" button disappears after the page reload', () => {
-    cy.visit("/add_remove_elements/");
-    cy.get(".example")
-      .find("button")
-      .then((btn) => {
-        expect(btn).to.contain("Add Element");
+        expect(btn).to.contain(
+          addRemoveElements.NAMES.addElementButtonuttonName
+        );
         cy.wrap(btn).click();
         cy.reload();
         cy.wrap(btn).should("not.exist");
       });
   });
+});
 
-  it('Verify "Delete" button disappears when clicked', () => {
-    cy.visit("/add_remove_elements/");
-    cy.contains("Add Element").click();
-    cy.get("#elements").should("contain", "Delete");
-    cy.get("#elements .added-manually")
-      .should("be.visible")
-      .click()
-      .should("not.exist");
+it("Click add element button 10times", () => {
+  cy.visit("/add_remove_elements/");
+  Cypress._.times(10, () => {
+    cy.contains("Add Element").click({ force: true }, { timeout: 400 });
   });
-
-  it("Click add element button 10times", () => {
-    cy.visit("/add_remove_elements/");
-    Cypress._.times(10, () => {
-      cy.contains("Add Element").click({ force: true }, { timeout: 400 });
-    });
-    cy.get(".added-manually").its("length").should("eq", 10);
-  });
+  cy.get(".added-manually").its("length").should("eq", 10);
 });
--- cypress/e2e/base.cy.js
+++ cypress/e2e/base.cy.js
@@ -1,12 +1,10 @@
 import { basePage } from "../Pages/Base";
 
-describe('The internet dashboard', () => {   //test suit
-  it('Verify clickable links in the page', () => {   //test case title
-    cy.visit('https://the-internet.herokuapp.com');
-    
-    cy.get('ul').first()
-    .find('a')
-    .its('length')
-    .should('eq',44);
-  })
-})
+describe("The internet dashboard", () => {
+  //test suit
+  it("Verify clickable links in the page", () => {
+    //test case title
+    cy.visit("/");
+    cy.get("ul").first().find("a").its("length").should("eq", 44);
+  });
+});
